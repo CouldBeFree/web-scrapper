a69d78b8c7098d74b6271ba4bc19b24eb5950e12
CouldBeFree <propokerstar90@gmail.com>
2019-09-22 15:22:04 +0300

    Get job description

--- index.js
+++ index.js
@@ -20,10 +20,19 @@ async function scrapeListings(page) {
     return listings;
 }
 
+async function sleep(miliseconds){
+    return new Promise(resolve => setTimeout(resolve, miliseconds))
+}
+
 async function scrapeJobDescriptions(listings, page){
     for(let i = 0; i < listings.length; i++){
         await page.goto(listings[i].url);
         const html = await page.content();
+        const $ = cheerio.load(html);
+        const jobDescription = $('#postingbody').text();
+        listings[i].jobDescription = jobDescription;
+        console.log(listings[i].jobDescription);
+        await sleep(1000);
     }
 }
 
